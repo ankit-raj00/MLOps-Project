304c2ff2ec05b32d9218593311a85f25844c904c
ankit-raj00 <ankitkumar5259211@gmail.com>
2025-12-23 17:58:05 +0530

    project structure ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Ankit Raj",
+    author_email="ankitkumar5259211@gmail.com",
+    packages=find_packages()
+)
